67adffba56eb70f30f7bb60577663533c8faae45
Prelle <p.elayne.terry@gmail.com>
2025-04-07 20:39:36 -0600

    restore and fix __dirname for creating file paths on deployed site

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
-//import path from 'node:path';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 import type { Request, Response } from 'express';
 import db from './config/connection.js'
 import { ApolloServer } from '@apollo/server';// Note: Import from @apollo/server-express
@@ -29,10 +30,12 @@ const startApolloServer = async () => {
   ));
 
   if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('../client/dist'));
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+    app.use(express.static(path.join(__dirname, '../../client/dist')));
 
     app.get('*', (_req: Request, res: Response) => {
-      res.sendFile('../client/dist/index.html');
+      res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
     });
   }
 
